c9cebb325a80ebce5a29292fb9ebdc672e4b1c34
Yann Hamon <yann@trailburning.com>
2013-08-06 16:22:28 +0200

    New php version

--- support/set-env.sh
+++ support/set-env.sh
@@ -3,15 +3,14 @@
 export S3_BUCKET="trailburning-heroku-packages"
 
 export LIBMCRYPT_VERSION="2.5.8"
-export PHP_VERSION="5.4.12"
+export PHP_VERSION="5.4.17"
 export APC_VERSION="3.1.10"
 export PHPREDIS_VERSION="2.2.2"
 export LIBMEMCACHED_VERSION="1.0.7"
 export MEMCACHED_VERSION="2.0.1"
-export NEWRELIC_VERSION="2.9.5.78"
 
 export NGINX_VERSION="1.4.1"
 
-export EC2_PRIVATE_KEY=~/.ec2/pk.pem
-export EC2_CERT=~/.ec2/cert.pem
-export EC2_URL=https://ec2.us-east-1.amazonaws.com
+export EC2_PRIVATE_KEY=~/.ec2/pk-F7SXD6OVMXKXQIA56O2FZ6N7MXWCR7QL.pem
+export EC2_CERT=~/.ec2/cert-F7SXD6OVMXKXQIA56O2FZ6N7MXWCR7QL.pem
+export EC2_URL=https://ec2.eu-west-1.amazonaws.com
